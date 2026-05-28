f670086c7c8b5b12bba37c1e06a5959be384b8a5
sini-max <sinitashinu678@gmail.com>
2026-05-28 23:55:46 +0530

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,11 +1,11 @@
 (function (window) {
   var speakWord = "Good Bye";
-  var byeSpeaker = {};
+  var ByeSpeaker = {};
 
-  byeSpeaker.speak = function (name) {
+  ByeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   };
 
-  window.byeSpeaker = byeSpeaker;
+  window.ByeSpeaker = ByeSpeaker;
 
 })(window);
